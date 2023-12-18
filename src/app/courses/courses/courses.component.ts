b4600d9d1d102b2069efa61848319d0627b7326a
Wesley Lopes <westleylopes4@gmail.com>
2023-12-18 18:35:29 -0300

    novos arquivos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { catchError, Observable, of } from 'rxjs';
 
 import { Course } from '../model/course';
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
 import { CoursesService } from './../services/courses.service';
 
 @Component({
@@ -15,15 +17,25 @@ export class CoursesComponent implements OnInit {
 
   //coursesService: CoursesService;
 
-  constructor(private coursesService: CoursesService) {
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+    ) {
     // this.courses = [];
     //this.coursesService = new CoursesService();
     this.courses$ = this.coursesService.list()
     .pipe(
       catchError(error => {
+        this.onError('Erro ao carregar cursos.');
         return of([])
       })
     );
+  }
+
+  onError(erroMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: erroMsg
+    });
   }
 
   ngOnInit(): void {
--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @NgModule({
   exports: [
@@ -10,6 +12,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatTableModule,
     MatToolbarModule,
     MatProgressSpinnerModule,
+    MatDialogModule,
   ],
 })
 export class AppMaterialModule {}
